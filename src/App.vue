56c3304d989eb9facd1b940e28f0c64049ef78ab
VladimirStega03 <114366736+VladimirStega03@users.noreply.github.com>
2022-10-20 04:16:03 +0300

    add file

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <CalculatorTest />
+  </div>
+</template>
+
+<script>
+
+import CalculatorTest from './components/CalculatorTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorTest
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
